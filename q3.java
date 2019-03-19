d16c62bdd2fb42b977efe0306a03a37c3d2e8c42
obli99 <44501265+obli99@users.noreply.github.com>
2019-03-19 22:10:51 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -5,19 +5,17 @@ public class q3 {
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	long number=600851475143L;
-		int max=0;
+		
 		for(int i=2;i<number;i++)
 		{
 			while(number%i==0)
 			{
-				System.out.println(i+" ");
 				number=number/i;
 				
 			}
 		}
 		if(number>2)
 		{
-			max=(int) number;
 			System.out.println(number);
 			
 		}
