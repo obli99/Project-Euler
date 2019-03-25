1ac6f827651256dee4719e21a90ddb445ff8d579
obli99 <44501265+obli99@users.noreply.github.com>
2019-03-25 18:52:57 +0530

    Update q5.java

--- q5.java
+++ q5.java
@@ -1,25 +1,27 @@
 package codex;
+import java.util.*;
 public class q5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=20;;i=i+20)
+		Scanner sc=new Scanner (System.in);
+for(int i=20;;i=i+20)
+{
+	int c=0;
+	for(int j=1;j<=20;j++)
+	{
+		if(i%j!=0)
 		{
-			int c=0;
-			for(int j=1;j<=20;j++)
-			{
-				if(i%j!=0)
-				{
-					c=1;
-					break;
-				}
-			}
-			if(c==0)
-			{
-				System.out.println("The smallest positive number that is evenly divisible by all of the number from 1 to 20 ="+i);
-				break;
-			}
+			c=1;
+			break;
 		}
+	}
+	if(c==0)
+	{
+		System.out.println("The smallest positive number that is evenly divisible by all of the number from 1 to 20 ="+i);
+		break;
+	}
+}
 	}
 
 }
